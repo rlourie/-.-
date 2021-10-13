57a136a77c2ebe04b0db7291a25fc9ed2b09ea9f
rlourie <super.sanek20142@yandex.ru>
2021-10-13 18:06:59 +0300

    Задание 3 полностью сделанна

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.sr = 0
 
     def rate_lecture(self, lecturer, course, grade):
         if isinstance(lecturer,
@@ -17,6 +18,19 @@ class Student:
         else:
             print("Ошибка")
             return
+
+    def __str__(self):
+        Lecturer.sr_grade(self)
+        result = f'Имя : {self.name}\nФамилия : {self.surname}\nСредняя оценка за дз : {self.sr}\n' \
+                 f'Курсы в процессе изучения: {" ".join(self.courses_in_progress)}\n' \
+                 f'Завершенные курсы: {"".join(self.finished_courses)}'
+        return result
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print("Это не студент")
+            return
+        return self.sr < other.sr
 
 
 class Mentor:
@@ -49,6 +63,12 @@ class Lecturer(Mentor):
         result = f'Имя : {self.name}\nФамилия : {self.surname}\nСредняя оценка за лекции : {self.sr}'
         return result
 
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print("Это не лектор")
+            return
+        return self.sr < other.sr
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -68,6 +88,11 @@ class Reviewer(Mentor):
 
 Alex = Student('Alex', 'Zvezdin', 'M')
 Alex.courses_in_progress += ['Python', 'SQL']
+Alex.finished_courses += ['Django']
+
+Artem = Student('Artem', 'Zvezdin1', 'M')
+Artem.courses_in_progress += ['Python', 'SQL']
+Artem.finished_courses += ['Django']
 
 Oleg = Reviewer('Oleg', 'Buligin')
 Oleg.courses_attached += ['Python', 'SQL']
@@ -75,13 +100,42 @@ Oleg.courses_attached += ['Python', 'SQL']
 Ne_Oleg = Lecturer('Ne_Oleg', 'Ne_Buligin')
 Ne_Oleg.courses_attached += ['Python', 'SQL']
 
+Artemiy = Lecturer('Artemiy', 'Ne_Buligin')
+Artemiy.courses_attached += ['Python', 'SQL']
+
 Oleg.rate_hw(Alex, 'Python', 10)
-Oleg.rate_hw(Alex, 'Python', 10)
-Oleg.rate_hw(Alex, 'Python', 10)
+Oleg.rate_hw(Alex, 'Python', 2)
+Oleg.rate_hw(Alex, 'Python', 8)
+Oleg.rate_hw(Alex, 'SQL', 9)
+
+Oleg.rate_hw(Artem, 'Python', 2)
+Oleg.rate_hw(Artem, 'Python', 2)
+Oleg.rate_hw(Artem, 'Python', 2)
+Oleg.rate_hw(Artem, 'SQL', 2)
+
 Alex.rate_lecture(Ne_Oleg, 'Python', 10)
 Alex.rate_lecture(Ne_Oleg, 'Python', 9)
 Alex.rate_lecture(Ne_Oleg, 'Python', 6)
 Alex.rate_lecture(Ne_Oleg, 'SQL', 10)
 
+Alex.rate_lecture(Artemiy, 'Python', 0)
+Alex.rate_lecture(Artemiy, 'Python', 9)
+Alex.rate_lecture(Artemiy, 'Python', 6)
+Alex.rate_lecture(Artemiy, 'SQL', 10)
+
 print(Ne_Oleg)
 print(Ne_Oleg.grades)
+
+print()
+
+print(Oleg)
+
+print()
+
+print(Alex)
+print(Alex.grades)
+
+print()
+
+print(Alex > Artem)
+print(Artemiy > Ne_Oleg)
